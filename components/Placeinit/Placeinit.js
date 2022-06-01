00eadffd4237012c6086c6d43964f64c9d562768
SmeraldaKa0s <anabella.lerner@gmail.com>
2022-06-01 16:06:15 -0300

    modifico archivos

---
+++ components/Placeinit/Placeinit.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+const Placeinit = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.titulo}>PlaceeeeeeINIT</h1>
+    </div>
+  );
+};
+
+export default Placeinit;
---
+++ components/Placeinit/index.js
@@ -0,0 +1,3 @@
+import Component from "./Placeinit"
+
+export default Component
