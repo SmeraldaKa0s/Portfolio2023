e502bea126d6b78ca4bde0e94fc99ed99916888a
SmeraldaKa0s <anabella.lerner@gmail.com>
2024-02-09 19:31:42 -0300

    global changes

--- pages/about/About.js
+++ pages/about/About.js
@@ -4,6 +4,7 @@ import Icon from "../../components/Icon";
 import Image from "next/image";
 import cv from "../../public/cv.pdf"
 
+
 const About = () => {
 
   /**
@@ -25,7 +26,6 @@ const About = () => {
   return (
     <Layout title="About">
       <section>
-        <span className={styles.progress}>WORK IN PROGRESS</span>
         <div className={styles.container}>
           <p className={styles.text1}>UX DESIGNER</p>
           <p className={styles.text2}>RESEARCHER</p>
@@ -35,14 +35,30 @@ const About = () => {
           </div>
           <p className={styles.text4}>DEVELOPER</p>
         </div>
-        <div className={styles.boxImage}>
-          {/* <Image
-            src="/Images/character.png"
-            width="200"
-            height="450"
+        <div className={styles.mainBox}>
+          <p className={styles.mainText}> Avocada a la transdiciplina. Me expreso investigando y diseñando productos cuidadosamente elaborados. 
+            Me gusta pensar que lo que creamos tiene que mejorar vidas y ser duradero. </p>
+        </div>
+        <div className={styles.aboutBox}>
+          <p className={styles.subtext}>COSAS QUE ME GUSTAN</p>
+          <p className={styles.about}>Actualmente estoy investigando los interticios en el diseño de producto que integre ia. </p>
+          <p className={`${styles.about} ${styles.margin}`}>Anteriormente, tuve diversos roles que ampliaron mis perspectiva a la hora de desarrollar tecnologia, 
+          haber pasado por el rol de qa analyst, frotend development y researcher me entrenaron los ojos y tendieron un puente hacia la constante busqueda de mejora y calidad.</p>
+          <p className={`${styles.about} ${styles.margin}`}>Antes de diseñar para la web me dedicaba a la bibliotecologia, las fuentes de informacion son muy importantes para mi. </p>
+          <p className={`${styles.about} ${styles.margin}`}>Y además estudie artes del fuego, filosofia, y artes electronicas, y cosas que ya no recuerdo. </p>
+          <p className={`${styles.about} ${styles.margin}`}>Todos estos temas tienen un impacto significativo en cómo inicio y desarrollo mis procesos creativos, marcando la pauta de cómo percibo el desarrollo web hoy en día.</p>
+          <p className={`${styles.about} ${styles.margin}`}>Cuando no estoy trabajando, normalmente estoy al aire libre, y tambien diseño! hago estudios paisajisticos enfocados en flora nativa, lo que me lleva a querer estar mucho tiempo en selvas,
+           bosques y diversidades vegetales para seguir reconociendo ese mundo de relaciones complejas; argentina -el mejor país del mundo- tiene muchos biomas por explorar. </p>
+        <div className={styles.myselfBox}>
+        <Image
+            src="/Images/alxportfolio.png"
+            width="500"
+            height="500"
             className={styles.image}
-            alt="personaje"
-          /> */}
+            alt="myself"
+          />
+        </div>
+           
         </div>
       </section>
 
