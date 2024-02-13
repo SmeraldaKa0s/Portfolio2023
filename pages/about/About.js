6d4c1fd12b265744ce61861a6c847fb66b595005
em3c2 <emiliacabralb@gmail.com>
2024-02-13 02:05:29 -0300

    added pokemon

--- pages/about/About.js
+++ pages/about/About.js
@@ -120,7 +120,7 @@ const About = () => {
             <p className={styles.text4}>ABOUT ME</p>
           </div>
           <div className={styles.imageConteiner}>
-          <Card image={"/Images/pokemon.png"} withBN />
+            <Card image={"/Images/pokemon.png"} withBN />
             <Card image={"/Images/pokemon.png"} withBN />
             <Card image={"/Images/pokemon.png"} withBN />
             <Card image={"/Images/principal.png"} withBN />
