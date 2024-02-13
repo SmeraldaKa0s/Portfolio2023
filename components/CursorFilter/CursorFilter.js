cde4c01903a8ab738cf9ce6dd6fb9c1049e80ddd
em3c2 <emiliacabralb@gmail.com>
2024-02-13 02:33:36 -0300

    added cleanup funtion and renaming

--- components/CursorFilter/CursorFilter.js
+++ components/CursorFilter/CursorFilter.js
@@ -2,24 +2,34 @@ import { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
 
 const CursorFilter = () => {
-  const [cor, setCor] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   const [width, setWidth] = useState(10000);
+
+  const handleFollow = (e) => {
+    setPosition({
+      x: Math.round(e.clientX),
+      y: Math.round(e.clientY),
+    });
+  };
 
   useEffect(() => {
     if (!window) return;
+    if (window.innerWidth < 600) return;
+
     setWidth(window.innerWidth * 2);
-    document.addEventListener("pointermove", (e) => {
-      setCor({
-        x: Math.round(e.clientX),
-        y: Math.round(e.clientY),
-      });
-    });
+
+    document.addEventListener("pointermove", handleFollow);
+    () => document.removeEventListener("pointermove", handleFollow);
   }, []);
 
   return (
     <div
       className={styles.cursor}
-      style={{ left: cor.x, top: cor.y, boxShadow: `0 0 0 ${width}px #000` }}
+      style={{
+        left: position.x,
+        top: position.y,
+        boxShadow: `0 0 0 ${width}px #000`,
+      }}
     />
   );
 };
