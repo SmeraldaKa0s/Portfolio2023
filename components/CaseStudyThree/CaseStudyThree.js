b0b661131cf5cef0b29b89997bf9dfc3ef3a8c8b
SmeraldaKa0s <anabella.lerner@gmail.com>
2024-03-06 13:35:41 -0300

    added primavera case study content

--- components/CaseStudyThree/CaseStudyThree.js
+++ components/CaseStudyThree/CaseStudyThree.js
@@ -2,11 +2,9 @@ import styles from "./styles.module.scss";
 import ImageNext from "../../components/ImageNext";
 
 const CaseStudyThree = ({ project }) => {
-    return (
-      <div className={`${styles.margin} ${styles.container}`}>
-            <p>CONSTRUCTION CASE STUDY</p>
-    
-        <div className={styles.card}>
+  return (
+    <div className={`${styles.margin} ${styles.container}`}>
+      <div className={styles.card}>
         <div className={styles.image}>
           <ImageNext
             src={project.image}
@@ -16,8 +14,134 @@ const CaseStudyThree = ({ project }) => {
           />
         </div>
       </div>
+      <div className={styles.content}>
+        <span>{project.number}</span>
+        <h2 className={styles.title}>{project.title}</h2>
+        {/*  <p>{caption}</p> */}
       </div>
-    );
-  };
-  
-  export default CaseStudyThree;
+      <div className={styles.boxInfo}>
+        <div className={styles.description}>
+          <p className={styles.main}>
+            Primavera es una ONG humanitaria que brinda valiosos programas de
+            educación para adultos, capacitación laboral y atención médica a su
+            comunidad. A pesar de su impacto positivo, la organización carecía
+            de una presencia en línea que facilitara el acceso a sus servicios.
+            El equipo de Primavera reconoció la necesidad de diseñar y
+            desarrollar su primer sitio web para brindar una mejor experiencia
+            de usuario y aumentar la interacción con su comunidad.
+          </p>
+        </div>
+        <div className={styles.boxRoles}>
+          <p className={styles.subtitle}>MY ROLES:</p>
+          <p className={styles.roles}>
+            UX/UI Designer ・ Brand Identity ・ Frontend Developer
+          </p>
+          <p className={styles.subtitle}>PLATFORM:</p>
+          <p className={styles.roles}>Mobile Web / Desktop</p>
+          <p className={styles.subtitle}>DATE:</p>
+          <p>Octubre 2022 - Marzo 2023</p>
+        </div>
+      </div>
+
+      <div className={`${styles.boxProblem} ${styles.margin}`}>
+        <div className={styles.roles}>
+          <p>01</p>
+          <h3>Challenge</h3>
+        </div>
+        <div>
+          <p className={styles.description}>
+            Identificamos dos desafíos importantes a abordar en el diseño del
+            sitio web. Primero, reconocimos la necesidad de ampliar la audiencia
+            más allá de las redes sociales y aumentar el tráfico de visitantes
+            fuera de estas plataformas. En segundo lugar, la ONG pretendía
+            transmitir una sensación de calidez y fraternalismo para atraer a
+            usuarios de todas las edades, considerando las diversas habilidades
+            digitales que podrían tener.
+          </p>
+        </div>
+      </div>
+      <div className={`${styles.boxProblem} ${styles.margin}`}>
+        <div className={styles.roles}>
+          <p>02</p>
+          <h3>Immersion</h3>
+        </div>
+        <div>
+          <p className={`${styles.description} ${styles.roles}`}>
+            Comencé adentrándome en la propuesta de valor de Primavera para
+            ganar claridad y alineación.
+          </p>
+          <p className={`${styles.description} ${styles.roles}`}>
+            Durante la fase de inmersión, llevé a cabo una sesión de
+            descubrimiento de medio día, donde abordamos las preguntas más
+            urgentes de la ONG. Luego utilizando los conocimientos recopilados
+            en esta sesión, establecimos el objetivo compartido.
+          </p>
+          <ul className={`${styles.description} ${styles.roles}`}>
+            What I did:
+            <li>・1:1 Interviews with Stakeholders</li>
+            <li>・Competitive Audit</li>
+            <li>・Brand Audit</li>
+          </ul>
+        </div>
+      </div>
+      <div className={`${styles.card} ${styles.margin}`}>
+        <div className={styles.image}>
+          <ImageNext src="/Images/Mapamental.jpg" width={900} height={700} />
+        </div>
+        <span className={"text-base text-zinc-400"}>
+          Mind map of the immersion phase to establish hierarchies and structure
+          elements by categories.
+        </span>
+      </div>
+      <div className={`${styles.boxProblem} ${styles.margin}`}>
+        <div className={styles.roles}>
+          <p>03</p>
+          <h3>Design Strategy</h3>
+        </div>
+        <div>
+          <p className={`${styles.description} ${styles.roles}`}>
+            Durante la fase de estrategia de diseño, establecimos objetivos
+            importantes para el producto: como compatibilidad con artículos
+            instantáneos de Facebook, un blog con carga previa de artículos en
+            la parte superior de la pagina, un buscador interno amigable que
+            permita encontrar rápidamente información sobre programas, eventos,
+            etc.
+          </p>
+          <p className={`${styles.description} ${styles.roles}`}>
+            También realicé una investigación comparativa para aprender de
+            nuestros principales competidores e identifiqué atributos clave de
+            nuestro público objetivo que se entregaron mediante protopersonas.
+          </p>
+        </div>
+      </div>
+      <div className={`${styles.boxProblem} ${styles.margin}`}>
+        <div className={styles.roles}>
+          <p>04</p>
+          <h3>User Experience</h3>
+        </div>
+        <div>
+          <p className={`${styles.description} ${styles.roles}`}>
+            Basándome en los conocimientos recopilados durante las fases de
+            inmersión y estrategia de diseño, diseñé un sitio web atractivo y
+            fácil de usar. Para transmitir una sensación de calidez y
+            proporcionar una buena legibilidad, incorporamos elementos de diseño
+            como una paleta de colores cuidadosamente elaborada, espacios claros
+            significativos e imágenes con miembros de la comunidad. Además,
+            optimizamos el sitio web con enfoque mobile first e incluyendo
+            varios dispositivos como tablets y portátiles, para ofrecer una
+            excelente experiencia a cada usuario.
+          </p>
+          <ul className={`${styles.description} ${styles.roles}`}>
+            What I did:
+            <li>・User Flows</li>
+            <li>・Information Architecture</li>
+            <li>・Lo Fi and Hi Fi Wireframes</li>
+            <li>・1:1 Usability Testing with team members and users</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaseStudyThree;
