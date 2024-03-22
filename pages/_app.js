08adb3ea58a14a37d9d5fb8001364c6432fd78e3
Anabella Lerner <anabella.lerner@gmail.com>
2024-03-22 18:31:20 -0300

    added animation background;

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.scss";
+import { BackgroundGradientAnimation } from "../components/background-gradient-animation";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <BackgroundGradientAnimation containerClassName="fixed top-0 left-0 z-10 saturate-[.3] brightness-[.4]" gradientBackgroundStart="black" gradientBackgroundEnd="black" />
+      <div className="relative z-20">
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
