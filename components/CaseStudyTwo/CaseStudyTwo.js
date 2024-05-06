eecde51b412293f609f49e28414e73d8d7ffdc19
Anabella Lerner <anabella.lerner@gmail.com>
2024-05-06 14:51:49 -0300

    modified caseStudyTwo

--- components/CaseStudyTwo/CaseStudyTwo.js
+++ components/CaseStudyTwo/CaseStudyTwo.js
@@ -2,11 +2,11 @@ import styles from "./styles.module.scss";
 import ImageNext from "../../components/ImageNext";
 
 const CaseStudyTwo = ({ project }) => {
-    return (
-      <div className={`${styles.margin} ${styles.container}`}>
-       <p>CONSTRUCTION CASE STUDY</p>
-   
-        <div className={styles.card}>
+  return (
+    <div className={`${styles.margin} ${styles.container}`}>
+      <p className={styles.text}>CONSTRUCTION CASE STUDY</p>
+
+      <div className={styles.card}>
         <div className={styles.image}>
           <ImageNext
             src={project.image}
@@ -16,8 +16,32 @@ const CaseStudyTwo = ({ project }) => {
           />
         </div>
       </div>
+
+      <div className={styles.boxInfo}>
+        <div className={styles.description}>
+          <p className={styles.main}>
+            MYXA, empresa técnica de tableros contra incendio, buscaba modernizar su presencia online y mejorar la experiencia de sus clientes.
+
+            Empezamos desde cero, definiendo la marca y creando una identidad visual completa. Diseñamos y desarrollamos una página web atractiva y funcional, además de elementos como tarjetas de presentación y prendas laborales.
+          </p>
+        </div>
+        <div className={styles.boxRoles}>
+          <p className={styles.subtitle}>MY ROLES:</p>
+          <p className={styles.roles}>
+            Product Design ・ Brand Identity ・ UX/UI Design
+          </p>
+          <p className={styles.subtitle}>TEAMMATE:</p>
+          <p className={styles.roles}>
+            Emilia Cabral - Web Developer
+          </p>
+          <p className={styles.subtitle}>PLATFORM:</p>
+          <p className={styles.roles}>Desktop / Mobile Web</p>
+          <p className={styles.subtitle}>DATE:</p>
+          <p>Octubre 2023 - Marzo 2024</p>
+        </div>
       </div>
-    );
-  };
-  
-  export default CaseStudyTwo;
+    </div>
+  );
+};
+
+export default CaseStudyTwo;
